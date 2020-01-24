d0a26c9b675bf69eaf6ca8ba22f2c6e072fa6b29
Hunter McIntosh <hm547416@ohio.edu>
2020-01-24 15:20:39 -0500

    Hello git program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Hunter McIntosh
+ *        Date: 1/24/20
+ * Description: git hub lab 2 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
